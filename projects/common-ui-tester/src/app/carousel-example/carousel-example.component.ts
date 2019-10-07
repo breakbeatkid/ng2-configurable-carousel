e8a5889e062b425faadce89da440cd5fed963187
richard.easton <richard.easton@crowderconsult.co.uk>
2019-10-07 13:45:25 +0100

    fix: better control on carousel example

--- projects/common-ui-tester/src/app/carousel-example/carousel-example.component.ts
+++ projects/common-ui-tester/src/app/carousel-example/carousel-example.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-carousel-example',
   templateUrl: './carousel-example.component.html',
   styleUrls: ['./carousel-example.component.scss']
 })
-export class CarouselExampleComponent implements OnInit {
+export class CarouselExampleComponent {
+  componentWidth = '100';
+  numberOfItems = 15;
+  items: any[];
 
-  constructor() { }
+  dynamicDisplaySize = true;
+  itemsDisplayed: number;
+  infiniteScroll = true;
 
-  ngOnInit() {
+  constructor() {
+    this.buildList();
   }
 
+  private buildList() {
+    this.items = [];
+    for (let i = 1; i <= this.numberOfItems; i++) {
+      this.items.push({ title: `Item ${i}` });
+    }
+  }
 }
